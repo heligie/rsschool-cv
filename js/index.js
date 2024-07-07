8d5cfbc7d8a0cd37b85c51f8f856a632b0d50d59
heligie <heligiette@gmail.com>
2024-07-07 11:50:16 +0300

    feat: implement the burger menu to toggle a navigation

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 const ranges = document.querySelectorAll('.skills__range-bar');
+const header = document.querySelector('.header');
+const burgerMenu = document.querySelector('.header__toggle');
 
+burgerMenu.addEventListener('click', () => header.classList.toggle('is-open'));
 const setRangeValue = () => ranges.forEach((range) => range.style.width = range.dataset.percent);
 
 setRangeValue();
